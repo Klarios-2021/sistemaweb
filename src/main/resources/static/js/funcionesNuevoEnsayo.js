7cf27efdb792b0ed313c468e04356054af90ca7e
Gonzalo Ávila <44387608+Gonzalo-Avila@users.noreply.github.com>
2021-08-05 16:28:02 -0300

    Update funcionesNuevoEnsayo.js

--- src/main/resources/static/js/funcionesNuevoEnsayo.js
+++ src/main/resources/static/js/funcionesNuevoEnsayo.js
@@ -3,8 +3,6 @@ function fillSelect(){
     document.getElementById("selectEstablecimiento").hidden =
         document.getElementById("laboratorio").value != "" ? false : true
 
-    document.getElementById("establecimiento").value =
-
 
     /*document.getElementById("hasta").value = null;
     document.getElementById("nombrePaciente").value = null;*/
