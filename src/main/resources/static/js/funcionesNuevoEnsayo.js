f16ffe2797ce313830c8ba84bf673fdc07a0f66b
Gonzalo Ávila <44387608+Gonzalo-Avila@users.noreply.github.com>
2021-08-05 17:44:20 -0300

    Fix iconos

--- src/main/resources/static/js/funcionesNuevoEnsayo.js
+++ src/main/resources/static/js/funcionesNuevoEnsayo.js
@@ -1,9 +1,18 @@
 function fillSelect(){
 
+    /*
     document.getElementById("selectEstablecimiento").hidden =
         document.getElementById("laboratorio").value != "" ? false : true
 
+     */
 
+    //var laboratorioElegido = document.getElementById("laboratorio").value
+
+    /*<![CDATA[*/
+    var laboratorios = [[${laboratorios}]]
+    /*]]>*/
+    console.log(laboratorios.length)
+    document.getElementById("asd").innerHTML = "5"
     /*document.getElementById("hasta").value = null;
     document.getElementById("nombrePaciente").value = null;*/
 }
